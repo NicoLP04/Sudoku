8960325df29dec708af187dc9d2751492dfebf5b
leo habets <leo.habets@epita.fr>
2023-12-04 13:10:46 +0100

    removed declaration of load_image & save_texture, in grid.c file

--- Image/GridDetection/grid.c
+++ Image/GridDetection/grid.c
@@ -296,40 +296,6 @@ Square FindBestSquare(List* squarelist)
 }
 
 
-SDL_Surface* load_image(const char* path)
-{
-	SDL_Surface* temp = IMG_Load(path);
-	if (temp == NULL)
-		errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_Surface* ret = SDL_ConvertSurfaceFormat(temp, SDL_PIXELFORMAT_RGB888,
-            0);
-	if (ret == NULL)
-		errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-	SDL_FreeSurface(temp);
-
-	return ret;
-}
-
-
-void save_texture(const char* file_name, SDL_Renderer* renderer,
-        SDL_Texture* texture)
-{
-    SDL_Texture* target = SDL_GetRenderTarget(renderer);
-    SDL_SetRenderTarget(renderer, texture);
-    int width, height;
-    SDL_QueryTexture(texture, NULL, NULL, &width, &height);
-    SDL_Surface* surface = SDL_CreateRGBSurface(0, width, height, 32,
-            0, 0, 0, 0);
-    SDL_RenderReadPixels(renderer, NULL, surface->format->format,
-            surface->pixels, surface->pitch);
-    IMG_SavePNG(surface, file_name);
-    SDL_FreeSurface(surface);
-    SDL_SetRenderTarget(renderer, target);
-}
-
-
 int main(int argc, char** argv)
 {
     if (argc != 2)
